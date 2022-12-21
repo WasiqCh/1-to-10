c1308a0bad13cf8eea2db27d9590581aa94b6125
WasiqCh <wasiqch94@gmail.com>
2022-12-22 00:34:55 +0500

    done hello world code

--- 1 to 10.cpp
+++ 1 to 10.cpp
@@ -2,22 +2,6 @@
 using namespace std;
 
 int main(){
-	int num;
-	char again='y';
-	while(again == 'y' || again == 'Y')
-	{
-		cout<<"Enter a number between 1 to 10: ";
-		cin>>num;
-		if(num<=10){
-			cout<<"Program worked successfully. :)";
-			return 0;
-		}
-		else if(num>10){
-			cout<<"Invalid choice"<<endl;
-		cout<<"Try again y/n: ";
-		cin>>again;
-		}
-		// 
-	}
+	cout<<"\n\nHello world";
 	return 0;
 }
