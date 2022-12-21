57ea2c6d63c93a2a5a4b21a84f5aaac6f7bf56dd
WasiqCh <wasiqch94@gmail.com>
2022-12-22 00:02:42 +0500

    first commit

---
+++ 1 to 10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num;
+	char again='y';
+	while(again == 'y' || again == 'Y')
+	{
+		cout<<"Enter a number between 1 to 10: ";
+		cin>>num;
+		if(num<=10){
+			cout<<"Program worked successfully. :)";
+			return 0;
+		}
+		else if(num>10){
+			cout<<"Invalid choice"<<endl;
+		cout<<"Try again y/n: ";
+		cin>>again;
+		}
+	}
+	return 0;
+}
